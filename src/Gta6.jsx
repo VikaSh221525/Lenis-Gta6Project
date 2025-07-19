f5901e7bcb925b6b76c3040536473f2195c41222
VikaSh221525 <vikassharma221525@gmail.com>
2025-07-19 23:57:25 +0530

    Animations&Scetion2

--- src/Gta6.jsx
+++ src/Gta6.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import React, { useState } from 'react'
-import {ArrowDown} from 'lucide-react'
+import { ArrowDown } from 'lucide-react'
 
 const Gta6 = () => {
     const [showContent, setshowContent] = useState(false);
@@ -31,18 +31,18 @@ const Gta6 = () => {
             })
     })
 
-    useGSAP(()=>{
+    useGSAP(() => {
         const main = document.querySelector(".main");
-        main?.addEventListener('mousemove', function(e){
-            const xmove = (e.clientX / window.innerWidth - 0.5)*40;
-            gsap.to(".imagesdiv .text",{
+        main?.addEventListener('mousemove', function (e) {
+            const xmove = (e.clientX / window.innerWidth - 0.5) * 40;
+            gsap.to(".imagesdiv .text", {
                 x: `${xmove * 0.4}%`
             })
-            gsap.to(".imagesdiv .sky",{
+            gsap.to(".imagesdiv .sky", {
                 x: xmove
             })
-            gsap.to(".imagesdiv .bg",{
-                x: xmove*1.7
+            gsap.to(".imagesdiv .bg", {
+                x: xmove * 1.7
             })
 
         })
@@ -81,7 +81,7 @@ const Gta6 = () => {
                 </svg>
             </div>
 
-            {showContent && <div className='main w-full '>
+            {showContent && <div className='main w-full text-white'>
                 <div className='w-full h-screen bg-zinc-900 text-white pricedown'>
                     <div className="navbar w-full py-10 px-10 absolute top-0 left-0 z-10">
                         <div className='logo flex items-center-safe gap-5'>
@@ -97,20 +97,37 @@ const Gta6 = () => {
                         <img className='sky w-full h-full object-cover absolute top-0 left-0 scale-110' src="./sky.png" alt="" />
                         <img className='bg w-full h-full object-cover absolute top-0 left-0 scale-105' src="./bg.png" alt="" />
                         <div className="text absolute top-10 left-1/2 -translate-x-1/2 ">
-                        <h1 className='text-8xl leading-none -ml-10'>grand</h1>
-                        <h1 className='text-8xl ml-15 leading-none'>theft</h1>
-                        <h1 className='text-8xl leading-none -ml-10'>auto</h1>
-                    </div>
+                            <h1 className='text-8xl leading-none -ml-10'>grand</h1>
+                            <h1 className='text-8xl ml-15 leading-none'>theft</h1>
+                            <h1 className='text-8xl leading-none -ml-10'>auto</h1>
+                        </div>
                         <img className='character absolute h-170 -bottom-[22%] left-1/2 -translate-x-40' src="./girlbg.png" alt="" />
                     </div>
                     <div className='bottomnav absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t from-black to-transparent'>
                         <div className='flex gap-4'>
-                            <ArrowDown size={30}/>
+                            <ArrowDown size={30} />
                             <h3 className='font-sans text-xl'>Scroll Down</h3>
                         </div>
                         <img className='h-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src="./ps5.png" alt="" />
                     </div>
-                    
+
+                </div>
+
+                <div className='w-full h-screen px-10 bg-zinc-900 flex items-center justify-between'>
+                    <div className='box w-full h-[80%] flex'>
+                        <div className='limg w-1/2 h-full relative'>
+                            <img className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-170' src="./imag.png" alt="" />
+                        </div>
+                        <div className='rg pricedown w-1/2 flex flex-col gap-10'>
+                            <div>
+                                <h1 className='text-7xl '>Still Running</h1>
+                                <h1 className='text-7xl '>Not Hunting</h1>
+                            </div>
+                            <p className='font-sans w-[80%]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis dolorum ratione accusantium modi pariatur doloribus dignissimos deserunt ipsam ut autem eaque, enim dolores explicabo? Aliquam harum sit quisquam dolores dolorem, suscipit neque ipsam, eveniet ullam provident facere laudantium quaerat et. Necessitatibus incidunt maxime sapiente tempora exercitationem perferendis quisquam officia soluta.</p>
+                            <p className='font-sans w-[80%] '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis dolorum ratione accusantium modi pariatur doloribus dignissimos deserunt ipsam ut autem eaque, enim dolores explicabo? </p>
+                            <button className='bg-yellow-500 text-black py-4 px-3 text-xl w-fit'>Download Now</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             }
